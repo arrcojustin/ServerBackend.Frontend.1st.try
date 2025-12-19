1909ee3d5227a1d07cfb8e8df479de5663c7ca9c
arrcojustin <justin@arrco.top>
2025-12-19 20:54:11 +0800

    Refactor main.h and optimize CMakeLists.txt
    fix: Fixed Linux linking error: Removed "ws2_32" from Unix link libraries to prevent build failure.
    refactor: Removed redundant g3log header inclusions.
    Other?
    I don't know()

---
+++ backend/build/main.h
@@ -0,0 +1,49 @@
+#pragma once
+
+//标准库
+#include <stdio.h>
+#include <iostream>
+#include <atomic>
+#include <string>
+#include <algorithm>
+
+//g3log库
+#include <g3log/g3log.hpp>
+#include <g3log/logworker.hpp>
+#include <g3log/loglevels.hpp>
+#include <g3log/logmessage.hpp>
+
+#ifdef _WIN32 // WINDOWS环境所需文件
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+#include <WinSock2.h>
+#include <windows.h>
+#include <ws2tcpip.h>
+
+#pragma comment(lib, "WS2_32.lib")
+
+int startup_win(unsigned short port);
+
+//Windows初始化函数
+#define STARTUP_FUNC(port) startup_win(port)
+#define accept_request accept_request_win
+
+#else // Linux/POSIX Sockets 头文件
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <errno.h>
+#include <arpa/inet.h> 
+#include <cstring>
+#include <pthread.h>
+#include <signal.h>
+
+int startup_posix(unsigned short port);
+
+// Linux 初始化函数 (通常为空操作)
+#define STARTUP_FUNC(port) startup_posix(port)
+#define accept_request accept_request_posix
+#endif
--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -1,37 +1,7 @@
-﻿#include <stdio.h>
-#include <iostream>
-#include <atomic>
-#include <string>
-#include <algorithm>
-#include <g3log/g3log.hpp>
-#include <g3log/logworker.hpp>
-#include <g3log/loglevels.hpp>
-#include <g3log/logworker.hpp>
-#include <g3log/logmessage.hpp>
+﻿#include "main.h"
+//头文件
 static std::atomic<bool> g_should_exit{ false };
 static int server_sock = -1;
-#ifdef _WIN32
-#include <WinSock2.h>
-#include <windows.h>
-#pragma comment(lib, "WS2_32.lib")
-// Windows 初始化函数
-#define STARTUP_FUNC(port) startup_win(port)
-#define accept_request accept_request_win
-#else
-// Linux/POSIX Sockets 头文件
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <unistd.h>
-#include <errno.h>
-#include <arpa/inet.h> 
-#include <cstring>
-#include <pthread.h>
-#include <signal.h>
-// for close()
-// Linux 初始化函数 (通常为空操作)
-#define STARTUP_FUNC(port) startup_posix(port)
-#define accept_request accept_request_posix
-#endif
 #ifdef _WIN32
 BOOL WINAPI ConsoleHandler(DWORD signal) {
 	if (signal == CTRL_C_EVENT || signal == CTRL_CLOSE_EVENT) {
